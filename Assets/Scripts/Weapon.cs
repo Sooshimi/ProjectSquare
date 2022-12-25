1c4445ccae857896cdd6586a01a71f34edf521df
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2022-12-25 21:54:26 +0000

    weapon rotation with mouse

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -12,6 +12,19 @@ public class Weapon : MonoBehaviour
         player = FindObjectOfType<Player>();
     }
 
+    void Update()
+    {
+        // Rotate weapon to point towards mouse direction
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = rotation;
+
+        // Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; 
+        // float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; 
+        // transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset);
+    }
+
     public void Shoot()
     {
         player.Knockback(knockBackAmount);
